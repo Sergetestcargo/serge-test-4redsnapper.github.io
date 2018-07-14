bb7e216f6b0668ac300d37c6a6960f658cfed652
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2018-07-14 12:33:03 +0300

    ng-app js format numbers fn

--- js/statsApp.js
+++ js/statsApp.js
@@ -3,7 +3,19 @@ angular.module("statsApp", [])
 		// just setting random score values for demo:
 		$scope.score = {
 			patents: 313,
-			followers: 1000200,
+			followers: 750200,
 			following: 361
+		}
+		$scope.formatNum = function (num) {
+		  if (num >= 1000000000) {
+		     return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'G';
+		  }
+		  if (num >= 1000000) {
+		     return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
+		  }
+		  if (num >= 1000) {
+		     return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
+		  }
+		  return num;
 		}		
 	});
